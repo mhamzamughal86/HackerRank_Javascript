989a2a1d10c76781c781f4a6bad0ec2f7472afd1
Muhammad Saeed <Muhammad.Saeed@CLICK.aero>
2021-03-23 12:09:17 +0400

    https://www.hackerrank.com/challenges/chocolate-feast/problem

---
+++ chocolateFeast.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the chocolateFeast function below.
+function chocolateFeast(budget, price, promo) {
+  let chocolateEat = Math.floor((budget / price));
+  let chocolateWrapper = chocolateEat;
+
+  while (chocolateWrapper >= promo) {
+    chocolateEat = chocolateEat + Math.floor((chocolateWrapper / promo));
+    chocolateWrapper = (chocolateWrapper%promo) + Math.floor((chocolateWrapper / promo));
+  }
+  return chocolateEat;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const t = parseInt(readLine(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const ncm = readLine().split(' ');
+
+        const n = parseInt(ncm[0], 10);
+
+        const c = parseInt(ncm[1], 10);
+
+        const m = parseInt(ncm[2], 10);
+
+        let result = chocolateFeast(n, c, m);
+
+        ws.write(result + "\n");
+    }
+
+    ws.end();
+}
